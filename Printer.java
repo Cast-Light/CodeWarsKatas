6a8e9ce97aea09bb44675447fe653bc0c672302e
Christian <christian.castellanos86@gmail.com>
2019-05-22 11:33:19 -0500

    Update Printer.java

--- Printer.java
+++ Printer.java
@@ -22,3 +22,19 @@ public class Printer {
         return s;
     }// end printerError
 }// end class
+
+Tests
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class PrinterTest {
+    @Test
+    public void test() {
+        System.out.println("printerError Fixed Tests");
+        String s="aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz";
+        assertEquals("3/56", Printer.printerError(s));
+    }// end test
+}// end PrinterTest
